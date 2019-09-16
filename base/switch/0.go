4469e06d0735437e96f6d46be328bad74b3763e8
heropoo <aiyouyou1000@163.com>
2019-09-16 10:34:05 +0800

    base

---
+++ base/switch/0.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	letter := "i"
+	switch letter {
+	case "a", "e", "i", "o", "u": //multiple expressions in case
+		fmt.Println("vowel")
+	default:
+		fmt.Println("not a vowel")
+	}
+}
