38bd24e4997225a11ca8772bbc15e5bb2c72bf14
heropoo <aiyouyou1000@163.com>
2019-09-30 18:43:58 +0800

    for

---
+++ base/for/2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+outer:
+    for i := 0; i < 3; i++ {
+        for j := 1; j < 4; j++ {
+            fmt.Printf("i = %d , j = %d\n", i, j)
+            if i == j {
+                break outer
+            }
+        }
+    }
+}
---
+++ base/if/1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+    if num :=10; num % 2 == 0 {
+        fmt.Println(num, "is even")
+    }else {
+        fmt.Println(num, "is odd")
+    }
+}
