3811eb918481db774730082f027df818bd77cba3
heropoo <aiyouyou1000@163.com>
2020-05-18 18:37:59 +0800

    switch case

---
+++ base/switch/finger2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch finger := 8; finger {
+	case 1:
+		fmt.Println("Thumb")
+	case 2:
+		fmt.Println("Index")
+	case 3:
+		fmt.Println("Middle")
+	case 4:
+		fmt.Println("Ring")
+	case 5:
+		fmt.Println("Pinky")
+	default: //default case
+		fmt.Println("incorrect finger number")
+	}
+}
