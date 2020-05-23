c2d0903f48ba4d461ffda4a79d2a27f7b1aba2c0
heropoo <aiyouyou1000@163.com>
2020-05-23 22:42:12 +0800

    goroutine

--- goroutine/1.go
+++ goroutine/1.go
@@ -7,8 +7,8 @@ import (
 
 func say(s string) {
 	for i := 0; i < 5; i++ {
-			time.Sleep(100 * time.Millisecond)
-			fmt.Println(s)
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
 	}
 }
 
--- goroutine/2.go
+++ goroutine/2.go
@@ -1,27 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func sum(s []int, c chan int){
+func sum(s []int, c chan int) {
 	sum := 0
-	for _, v := range s{
+	for _, v := range s {
 		sum += v
 		fmt.Println(v)
-	} 
-	c <- sum	//把 sum 发送到通道 c
+	}
+	c <- sum //把 sum 发送到通道 c
 }
 
-func main(){
-	s := []int{7,2,8,213,11,4}
+func main() {
+	s := []int{7, 2, 8, 213, 11, 4}
 
 	c := make(chan int)
-	
+
 	go sum(s[:len(s)/2], c)
 	go sum(s[len(s)/2:], c)
 
-	x, y := <- c, <- c   // 从通道 c 中接收
+	x, y := <-c, <-c // 从通道 c 中接收
 
 	fmt.Println(x, y, x+y)
 
