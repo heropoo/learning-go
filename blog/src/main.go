3049893936c6bffea8cfd9c8cf3cb343f074ef34
heropoo <aiyouyou1000@163.com>
2020-05-26 00:11:35 +0800

    json

--- blog/src/main.go
+++ blog/src/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type JsonData struct {
+	Code int
+	Msg  string
+	data string
+}
 
 func main() {
 	http.HandleFunc("/", showIndex)
@@ -15,7 +22,8 @@ func main() {
 
 	fmt.Println("Listen and serve on http://127.0.0.1:8080")
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	//err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		log.Fatal("Listen and serve: ", err.Error())
@@ -24,7 +32,7 @@ func main() {
 
 //首页
 func showIndex(w http.ResponseWriter, r *http.Request) {
-	data, err := ioutil.ReadFile("public/index.html")
+	data, err := ioutil.ReadFile("../public/index.html")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
@@ -35,11 +43,19 @@ func showIndex(w http.ResponseWriter, r *http.Request) {
 
 // 登录
 func login(w http.ResponseWriter, r *http.Request) {
-
 	io.WriteString(w, "Login")
 }
 
 //注销
 func logout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Logout")
+	//fmt.Fprintf(w, "Logout")
+	d := &JsonData{}
+	d.Code = 0
+	d.Msg = "Ok"
+	d.data = "logout"
+	data, _ := json.Marshal(d)
+
+	w.Header().Add("Content-Type", "application/json")
+	//fmt.Fprintf(w, string(data))
+	w.Write([]byte(data))
 }
