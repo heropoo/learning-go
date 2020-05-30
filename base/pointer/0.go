271d3a2cef382cf0cc45c22536fdf8a832f05424
heropoo <aiyouyou1000@163.com>
2020-05-30 13:58:15 +0800

    test

---
+++ base/pointer/0.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//指针取值
+	a := 10
+	b := &a // 取变量a的地址，将指针保存到b中
+	fmt.Printf("type of b:%T\n", b)
+	c := *b // 指针取值（根据指针去内存取值）
+	fmt.Printf("type of c:%T\n", c)
+	fmt.Printf("value of c:%v\n", c)
+}
--- base/pointer/1.go
+++ base/pointer/1.go
@@ -6,5 +6,6 @@ func main() {
 	b := 255
 	var a *int = &b
 	fmt.Printf("Type of a is %T\n", a)
-	fmt.Println("Address of b is ", a)
+	fmt.Printf("Address of b is %p\n", a)
+	fmt.Println(&a)
 }
